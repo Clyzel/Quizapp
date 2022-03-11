76130ed8c63e3050d7b9fb9062429c54f2385e58
Zel <cmorriswrk@gmail.com>
2022-03-10 21:53:46 -0600

    Set up that works

--- client/src/App.js
+++ client/src/App.js
@@ -16,19 +16,31 @@ function App() {
   return (
     <div className="App">
       <h1><center>History Quiz</center></h1>
-      {/* <h2><center>{JSON.stringify(data.results[0].question)}</center></h2> */}
-      {/* <center><Button answer={"True"} /></center>
-      <center><Button answer={"False"} /></center> */}
-
-      {/* <center><button>Next Question</button></center>
-   */}
+  
       
       <>
-      <h5>{data.results[0].question}</h5>
-      <h5>{data.results[1].question}</h5>
-      <h5>{data.results[2].question}</h5>
-      <h5>{data.results[3].question}</h5>
-      <h5>{data.results[4].question}</h5>
+      <h2><center>{data.results[0].question}</center></h2>
+
+      <center><button>True</button> <button>False</button></center>
+
+      <h2><center>{data.results[1].question}</center></h2>
+
+      <center><button>True</button> <button>False</button></center>
+
+      <h2><center>{data.results[2].question}</center></h2>
+
+      <center><button>True</button> <button>False</button></center>
+
+
+      <h2><center>{data.results[3].question}</center></h2>
+
+      <center><button>True</button> <button>False</button></center>
+
+      <h2><center>{data.results[4].question}</center></h2>
+
+      <center><button>True</button> <button>False</button></center>
+      <br></br>
+      <center><button>Reset Quiz</button></center>
       </>
      
     
